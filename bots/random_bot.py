a93d99453751748133154b010797cf9b3db53930
annalogie-create <62751653+annalogie-create@users.noreply.github.com>
2020-09-29 23:07:49 +0200

    Created random bot
    Created bot that decides randomly what action to take and in case of announcing what value to choose.

---
+++ bots/random_bot.py
@@ -0,0 +1,34 @@
+"""This bot decides randomly what action to take and in case of announcing what value to choose."""
+
+from lib.high_level_api import MaexchenHighLevelBotAPI
+import random
+import lib.tools as tools
+from time import sleep
+
+def random_answer(previous_turn):
+    rand = random.randint(0,1)
+    if rand and previous_turn is not None:
+        bot.accuse()
+    else:
+        all_values = tools.valid_game_values_lowest_to_highest()
+        if previous_turn is None:
+            values_to_choose_from = all_values
+        else:
+            value = previous_turn[1]
+            index = tools.value_to_rank(value)
+            values_to_choose_from = all_values[index+1:]  
+
+        bot.roll()                          
+        announce_value = random.choice(values_to_choose_from)
+        bot.announce(announce_value)
+
+bot = MaexchenHighLevelBotAPI("true_random_bot")
+bot.start()
+bot.register_callback(random_answer)
+
+while True:
+    try:
+        sleep(4)
+    except KeyboardInterrupt:
+        bot.close() 
+        exit(0)
