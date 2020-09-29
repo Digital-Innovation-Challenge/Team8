ad360f870e3542b93aa56217bc280f9f430b7307
Tobi <c4rgon4t@gmail.com>
2020-09-30 01:22:30 +0200

    aggressive and conservative strategy bots

---
+++ bots/aggressive_strategy_bot.py
@@ -0,0 +1,22 @@
+from lib.template_bot import TemplateBot
+from lib.tools import rank_to_value, value_to_rank
+
+
+class ConservativeStrategyBot(TemplateBot):
+    """
+    This bot always announces an aggressively high value.
+    """
+
+    # overridden
+    def callback_receiver(self, prev_turn):
+        if TemplateBot.exclude_trivialities(self, prev_turn, first_turn=(5, 4)):
+            return
+        value = prev_turn[1]
+        roll = self.bot.roll()
+        rank = max(value_to_rank((5, 4)), value_to_rank(value) + 1, value_to_rank(roll))
+        self.bot.announce(rank_to_value(rank))
+
+
+if __name__ == "__main__":
+    bot = ConservativeStrategyBot("aggressive_strategy")
+    bot.run()
