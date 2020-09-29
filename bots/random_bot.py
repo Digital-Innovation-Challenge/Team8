d5c386860a6923457d6b3efe9e9b94be3bca938f
Tobi <c4rgon4t@gmail.com>
2020-09-29 23:50:36 +0200

    formatting

--- bots/random_bot.py
+++ bots/random_bot.py
@@ -1,12 +1,14 @@
 """This bot decides randomly what action to take and in case of announcing what value to choose."""
 
-from lib.high_level_api import MaexchenHighLevelBotAPI
 import random
+
 import lib.tools as tools
+from lib.high_level_api import MaexchenHighLevelBotAPI
 from time import sleep
 
+
 def random_answer(previous_turn):
-    rand = random.randint(0,1)
+    rand = random.randint(0, 1)
     if rand and previous_turn is not None:
         bot.accuse()
     else:
@@ -16,11 +18,12 @@ def random_answer(previous_turn):
         else:
             value = previous_turn[1]
             index = tools.value_to_rank(value)
-            values_to_choose_from = all_values[index+1:]  
+            values_to_choose_from = all_values[index + 1:]
 
-        bot.roll()                          
+        bot.roll()
         announce_value = random.choice(values_to_choose_from)
         bot.announce(announce_value)
+
 
 bot = MaexchenHighLevelBotAPI("true_random_bot")
 bot.start()
@@ -30,5 +33,5 @@ while True:
     try:
         sleep(4)
     except KeyboardInterrupt:
-        bot.close() 
+        bot.close()
         exit(0)
