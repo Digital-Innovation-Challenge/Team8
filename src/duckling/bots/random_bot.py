4d6ecff003d1e41b9eac30b4f4f8020c437e1ee5
Florian <florian@flova.de>
2020-09-30 17:59:22 +0200

    Remove old bots

--- src/duckling/bots/random_bot.py
+++
@@ -1,6 +0,0 @@
-from duckling.bots.strategies import RandomStrategy
-from duckling.bots.strategy_bot import StrategyBot
-
-if __name__ == "__main__":
-    bot = StrategyBot("true_random_bot", RandomStrategy(accuse_percentage=0.1))
-    bot.run()
