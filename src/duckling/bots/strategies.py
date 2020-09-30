713884c5af0b9dcc554552857b1c9555791e3c95
Tobi <c4rgon4t@gmail.com>
2020-09-30 16:41:56 +0200

    Fix strategies.py (adjust for first turn and other small bugs)

--- src/duckling/bots/strategies.py
+++ src/duckling/bots/strategies.py
@@ -1,8 +1,7 @@
 import random
 
 import duckling.lib.tools as tools
-
-from src.duckling.machine_learning.lie_detctor.lie_detector import InferenceEngine
+from duckling.machine_learning.lie_detctor.lie_detector import InferenceEngine
 
 
 class AbstractStrategy:
@@ -35,7 +34,27 @@ class AbstractStrategy:
 
     def announce(self, prev_turns, our_roll):
         """
-        Override this function to decide what to announce
+        Call this function to find out what to announce
+        :param prev_turns: The previous turns as a list of 2-tuples containing the player and the announced roll
+        :param our_roll: Our current hidden roll as a 2-tuple
+        :return: The roll we should announce as a 2-tuple
+        """
+        if prev_turns is None or len(prev_turns) == 0:
+            return self.announce_first_turn(our_roll)
+        else:
+            return self.announce_later_turn(prev_turns, our_roll)
+
+    def announce_first_turn(self, our_roll):
+        """
+        Override this function to decide what to announce on the first turn
+        :param our_roll: Our current hidden roll as a 2-tuple
+        :return: The roll we should announce as a 2-tuple
+        """
+        pass
+
+    def announce_later_turn(self, prev_turns, our_roll):
+        """
+        Override this function to decide what to announce after the first turn
         :param prev_turns: The previous turns as a list of 2-tuples containing the player and the announced roll
         :param our_roll: Our current hidden roll as a 2-tuple
         :return: The roll we should announce as a 2-tuple
@@ -49,6 +68,7 @@ class AbstractMLStrategy(AbstractStrategy):
     """
 
     def __init__(self, model, version=0):
+        super(AbstractStrategy, self).__init__()
         self._detector_ie = InferenceEngine(model, version=version)
         self._version = version
 
@@ -70,7 +90,9 @@ class AbstractMLStrategy(AbstractStrategy):
 
 class MLStrategyFromOldStrategy(AbstractMLStrategy):
     def __init__(self, old_strategy, model, version=0):
-        super(AbstractMLStrategy, self).__init__(model, version)
+        super(AbstractMLStrategy, self).__init__()
+        self._detector_ie = InferenceEngine(model, version=version)
+        self._version = version
         self.old_strategy = old_strategy
 
     def announce(self, prev_turns, our_roll):
@@ -82,18 +104,19 @@ class RandomStrategy(AbstractStrategy):
     def __init__(self, accuse_percentage=0.1):
         self.accuse_percentage = accuse_percentage
 
-    def should_accuse_non_trivially(self, prev_turn):
+    def should_accuse_non_trivially(self, prev_turns):
         return random.random() < self.accuse_percentage
 
-    def announce(self, prev_turn, our_roll):
+    def announce_first_turn(self, our_roll):
+        return random.choice(tools.valid_game_values_lowest_to_highest())
+
+    def announce_later_turn(self, prev_turns, our_roll):
         all_values = tools.valid_game_values_lowest_to_highest()
-        if prev_turn is None:
-            values_to_choose_from = all_values
-        else:
-            value = prev_turn[1]
-            index = tools.value_to_rank(value)
-            values_to_choose_from = all_values[index + 1:]
-        announce_value = random.choice(values_to_choose_from)
+        prev_roll = prev_turns[-1][1]
+        value = prev_roll
+        index = tools.value_to_rank(value)
+        higher_values = all_values[index + 1:]
+        announce_value = random.choice(higher_values)
         return announce_value
 
 
@@ -101,7 +124,11 @@ class AggressiveStrategy(AbstractStrategy):
     def should_accuse_non_trivially(self, prev_turns):
         return False
 
-    def announce(self, prev_turns, our_roll):
+    def announce_first_turn(self, our_roll):
+        rank = max(tools.value_to_rank((5, 4)), tools.value_to_rank(our_roll))
+        return tools.rank_to_value(rank)
+
+    def announce_later_turn(self, prev_turns, our_roll):
         prev_roll = prev_turns[-1][1]
         rank = max(tools.value_to_rank((5, 4)), tools.value_to_rank(prev_roll) + 1, tools.value_to_rank(our_roll))
         return tools.rank_to_value(rank)
