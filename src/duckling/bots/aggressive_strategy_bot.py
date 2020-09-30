dcfdde52a70c3e942564be9282bb979cedd22815
Florian <florian@flova.de>
2020-09-30 17:39:39 +0200

    Clean old bots

--- src/duckling/bots/aggressive_strategy_bot.py
+++
@@ -1,6 +0,0 @@
-from duckling.bots.strategies import AggressiveStrategy
-from duckling.bots.strategy_bot import StrategyBot
-
-if __name__ == "__main__":
-    bot = StrategyBot("true_random_bot", AggressiveStrategy())
-    bot.run()
